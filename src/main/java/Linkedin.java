a898079c64d996e4e48e1a3c0c30fcdc50d957f6
Serhiy Polyakov <lua-lua@meta.ua>
2019-03-25 11:53:59 +0200

    Linkedin test

---
+++ src/main/java/Linkedin.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Linkedin {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:/MyDrivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.linkedin.com/");
+
+        WebElement loginEmail = driver.findElement(By.xpath("//input[@id='login-email']"));
+        loginEmail.sendKeys("testerok111333@gmail.com");
+
+        WebElement loginPass = driver.findElement(By.xpath("//input[@id='login-password']"));
+        loginPass.sendKeys("111333");
+
+        WebElement loginButton = driver.findElement(By.id("login-submit"));
+        loginButton.click();
+
+        if (driver.getPageSource().contains("Профиль"))
+        {
+            System.out.println("page is ok");
+        }
+        else
+        {
+            System.out.println("wrong page");
+        }
+        driver.quit();
+    }
+
+}
