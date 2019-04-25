932249129fd7bc410671f159f7635ebb4a9cadc1
Serhiy Polyakov <lua-lua@meta.ua>
2019-04-25 15:32:13 +0300

    My HW11 25.04

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -6,10 +6,16 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import page.LoginPage;
 
+/**
+ * Parent class for every test class.
+ */
 public class BaseTest {
     private WebDriver driver;
     protected LoginPage loginPage;
 
+    /**
+     * Before tests method to launch browser, go to "https://www.linkedin.com" and create new LoginPage.
+     */
     @BeforeMethod
     public  void beforeMethod() {
         System.setProperty("webdriver.chrome.driver", "C:/MyDrivers/chromedriver.exe");
@@ -19,6 +25,9 @@ public class BaseTest {
         loginPage = new LoginPage(driver);
     }
 
+    /**
+     * After tests method to close browser.
+     */
     @AfterMethod
     public  void afterMethod() {
         driver.quit();
